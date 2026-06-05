eacf2b1ba9b8e1c6454f12258ac9919f96d67abf
jaime <trabajojaimerotu@gmail.com>
2026-06-05 13:15:00 +0200

    chore: add travel-website service to docker-compose and travel_db to init-dbs

--- scripts/init-dbs.sh
+++ scripts/init-dbs.sh
@@ -7,6 +7,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
   CREATE DATABASE portfolio_db;
   -- Sample app
   CREATE DATABASE sample_app_db;
+  -- Travel website
+  CREATE DATABASE travel_db;
   -- Añade aqui más bases de datos para nuevos proyectos
 EOSQL
 
